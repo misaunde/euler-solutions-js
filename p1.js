b9dc7e6f6aefbae6b7c7ba097291863608c75fb3
Michael Saunders <michaelsaunders1@u.boisestate.edu>
2018-10-05 11:28:14 -0600

    adding lodash solution

--- p1.js
+++ p1.js
@@ -1,4 +1,5 @@
 const { performance } = require('perf_hooks');
+var _ = require('lodash');
 
 function sumNum(start = 0, end = Infinity, step = 1) {
     let n = 0;
@@ -8,6 +9,8 @@ function sumNum(start = 0, end = Infinity, step = 1) {
     return n;
 }
 
+
+
 let t0 = performance.now();
 console.log("sum: ", sumNum(0, 1000, 3) + sumNum(0, 1000, 5));
 let t1 = performance.now();
@@ -16,4 +19,21 @@ console.log("time: ", (t1 - t0) / 1000);
 //=============================
 // sum?:  266333
 // time:  0.0019370291233062743
+//=============================
+
+
+
+
+function lodash() {
+    return _.sum(_.range(0, 1000, 3)) + _.sum(_.range(0, 1000, 5));
+}
+
+t0 = performance.now();
+console.log("sum: ", lodash());
+t1 = performance.now();
+console.log("time: ", (t1 - t0) / 1000);
+
+//=============================
+// sum:  266333
+// time:  0.0004896161556243897
 //=============================
