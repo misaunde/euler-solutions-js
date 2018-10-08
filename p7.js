e1c86154018658b22f5fbc718d3235edf908c593
Michael Saunders <misaunde@users.noreply.github.com>
2018-10-07 21:46:42 -0600

    adding p7

---
+++ p7.js
@@ -0,0 +1,31 @@
+const { performance } = require('perf_hooks');
+var _ = require('lodash');
+
+function isPrime(num) {
+    for (var i = 2; i < num; i++) 
+        if (num % i == 0) 
+            return false;
+    return true;
+}
+
+function getPrime(position) {
+    let index = 1;
+    let value = 2;
+    while (true) {
+        value++;
+        if (isPrime(value)) 
+            index++;
+        
+        if (index == position)
+            return value;
+    }
+}
+
+let t0 = performance.now();
+console.log("prime: ", getPrime(10001))
+console.log("time: ", (performance.now() - t0) + " ms");
+
+//==============================
+// prime:  104743
+// time:  1783.8539319634438 ms
+//==============================
